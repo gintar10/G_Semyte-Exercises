be527d0052ff10b3f7b81f349cee5b5218b79b75
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-19 16:44:26 +0200

    Add PD2.2

---
+++ G-Semyte-Exercises/PD2/PD2.2/PD2_2.java
@@ -0,0 +1,11 @@
+package PD2_2;
+
+public class PD2_2 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("J     A     V     V     A");
+        System.out.println("J    A A     V   V     A A");
+        System.out.println("J   A A A     V V     A A A");
+        System.out.println("J  A     A     V     A     A");
+    }
+}
