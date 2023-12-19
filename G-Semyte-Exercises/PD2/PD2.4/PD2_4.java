90c10ed19ebe296ec47c4933c41de9d7fbf1b9f1
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-19 20:35:23 +0200

    Add PD2.4

---
+++ G-Semyte-Exercises/PD2/PD2.4/PD2_4.java
@@ -0,0 +1,16 @@
+package PD2_4;
+
+import java.util.Scanner;
+
+public class PD2_4 {
+    public static void main(String[] args) {
+        System.out.println("Please enter your name:");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+
+        System.out.println("Sveiki, " + name);
+    }
+}
+
+
