00002f2ad06897d40a89e4d5cce37f42ea997c18
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-26 17:03:13 +0200

    Add PD3.2

---
+++ G-Semyte-Exercises/PD3/PD3.2/PD3_2.java
@@ -0,0 +1,19 @@
+package PD3;
+
+public class PD3_2 {
+    public static void main(String[] args) {
+        byte myByte1 = 8;
+        short myShort1 = 16;
+        int mySum = (myByte1 + myShort1);
+        int myMultiplication = (myByte1 * myShort1);
+        int myDivision = (myShort1 / myByte1);
+        int myMinus = (myShort1 - myByte1);
+
+        System.out.println(myByte1);
+        System.out.println(mySum);
+        System.out.println(myShort1);
+        System.out.println(myMultiplication);
+        System.out.println(myDivision);
+        System.out.println(myMinus);
+    }
+}
