2795c4b44ba2442750075cd8b68b35479d1bf5b3
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-26 17:21:24 +0200

    Add PD3.3

---
+++ G-Semyte-Exercises/PD3/PD3.3/PD3_3.java
@@ -0,0 +1,13 @@
+package PD3;
+
+public class PD3_3 {
+    public static void main(String[] args) {
+        float a = 6.7f;
+        double b = 24;
+        float floatSum = a + a + a + a + a + a + a;
+        double doubleSum = b + b + b + b + b + b + b + b + b;
+
+        System.out.println(floatSum);
+        System.out.println(doubleSum);
+    }
+}
