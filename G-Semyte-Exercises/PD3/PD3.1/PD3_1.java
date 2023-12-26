d0ec15dcb53028d6eaa7d77dde8b32f042577a11
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-26 16:03:29 +0200

    Add PD3.1

---
+++ G-Semyte-Exercises/PD3/PD3.1/PD3_1.java
@@ -0,0 +1,23 @@
+package PD3;
+
+public class PD3_1 {
+    public static void main(String[] args) {
+        byte myByte = 32;
+        short myShort = 125;
+        int myInt = 166;
+        long myLong = 123456789L;
+        float myFloat = 20f;
+        double myDouble = 86.0;
+        boolean myBoolean = true;
+        char myChar = 65;
+
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myBoolean);
+        System.out.println(myChar);
+    }
+}
