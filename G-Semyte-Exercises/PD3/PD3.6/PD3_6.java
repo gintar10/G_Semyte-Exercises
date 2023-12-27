e7bf4cf80bc343b06278ca7ac901632102794a8b
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-27 20:12:40 +0200

    Add PD3.6

---
+++ G-Semyte-Exercises/PD3/PD3.6/PD3_6.java
@@ -0,0 +1,15 @@
+package PD3;
+
+public class PD3_6 {
+    public static void main(String[] args) {
+        Integer myInt = 99;
+        double myDouble = 4.44;
+        Character myChar = 'A';
+        Float myFloat = 4.71f;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myFloat);
+    }
+}
