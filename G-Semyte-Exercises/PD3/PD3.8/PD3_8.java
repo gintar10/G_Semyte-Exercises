776f9337dcc4f455b5ab3f25b8aaca2d23c74752
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-27 22:17:19 +0200

    Add PD3.8

---
+++ G-Semyte-Exercises/PD3/PD3.8/PD3_8.java
@@ -0,0 +1,12 @@
+package PD3;
+
+public class PD3_8 {
+    public static void main(String[] args) {
+        double myResult1 = ((7.5 * 6.5) - (4.5 * 3)) / (47.5 - 5.5);
+        double myResult2 = 12.78 / (15.4 - 2.75);
+        System.out.println(myResult1);
+        System.out.println(myResult2);
+        System.out.println(myResult1 == myResult2);
+    }
+}
+
