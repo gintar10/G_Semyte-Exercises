969dbc6c72a604447e8df763d6c08730ca571c26
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-28 15:54:09 +0200

    Add PD4.1

---
+++ G_Semyte-Exercises/PD4/PD4.1/PD4_1.java
@@ -0,0 +1,20 @@
+package PD4;
+
+public class PD4_1 {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 5;
+
+        int result = a + b;
+        int result2 = a - b;
+        int result3 = a * b;
+        int result4 = a / b;
+        int result5 = a % b;
+
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+    }
+}
