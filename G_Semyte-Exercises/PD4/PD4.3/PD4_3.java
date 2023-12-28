dc95b9c0d9a1874fb8c8304b10feab87376adb58
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-28 19:40:33 +0200

    Add PD4.3

---
+++ G_Semyte-Exercises/PD4/PD4.3/PD4_3.java
@@ -0,0 +1,14 @@
+package PD4;
+
+public class PD4_3 {
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean c = false;
+
+        boolean d = b && c;
+        boolean e = b || c;
+
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
