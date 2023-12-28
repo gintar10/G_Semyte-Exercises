08ac826912aee2bb2318a92d167b01042f329442
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-28 20:50:01 +0200

    Add PD4.5

---
+++ G_Semyte-Exercises/PD4/PD4.5/PD4_5.java
@@ -0,0 +1,12 @@
+package PD4;
+
+public class PD4_5 {
+    public static void main(String[] args) {
+        int time = 7;
+        if (time < 12) {
+            System.out.println("Morning");
+        } else {
+            System.out.println("Afternoon");
+        }
+    }
+}
