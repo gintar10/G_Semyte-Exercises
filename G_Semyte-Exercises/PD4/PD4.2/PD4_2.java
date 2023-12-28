3c1fbe53e535a786c7c7de89f9553a35e9bad9e4
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-28 16:38:41 +0200

    Add PD4.2

---
+++ G_Semyte-Exercises/PD4/PD4.2/PD4_2.java
@@ -0,0 +1,17 @@
+package PD4;
+
+public class PD4_2 {
+    public static void main(String[] args) {
+
+    int myNumber1 = 20;
+    int myNumber2 = 12;
+
+        System.out.println(myNumber1 == myNumber2);
+        System.out.println(myNumber1 != myNumber2);
+        System.out.println(myNumber1 > myNumber2);
+        System.out.println(myNumber1 < myNumber2);
+        System.out.println(myNumber1 >= myNumber2);
+        System.out.println(myNumber1 <= myNumber2);
+    }
+}
+
