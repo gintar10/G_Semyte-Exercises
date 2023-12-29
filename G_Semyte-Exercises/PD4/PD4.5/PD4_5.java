52edb580c2b6998b38b9b63897d6327fb67a7cef
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-29 18:46:16 +0200

    Add PD4.5

---
+++ G_Semyte-Exercises/PD4/PD4.5/PD4_5.java
@@ -0,0 +1,11 @@
+package PD4;
+
+public class PD4_5 {
+    public static void main(String[] args) {
+        int num1 = 30;
+        int num2 = 20;
+
+        int res = (num1 > num2) ? (num1 + num2) : (num1 - num2);
+        System.out.println(res);
+    }
+}
