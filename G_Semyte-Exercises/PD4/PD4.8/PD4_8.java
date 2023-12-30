0de346382223fb5e29cbc5a448a0e3f9cbd5c417
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-30 15:26:50 +0200

    Add PD4.8

---
+++ G_Semyte-Exercises/PD4/PD4.8/PD4_8.java
@@ -0,0 +1,32 @@
+package PD4;
+
+import java.util.Scanner;
+
+public class PD4_8 {
+    public static void main(String[] args) {
+
+        Scanner scannner = new Scanner(System.in);
+
+        System.out.println("Sienos ilgis m: ");
+        String ilgis = scannner.nextLine();
+
+        System.out.println("Sienos aukštis m: ");
+        String aukstis = scannner.nextLine();
+
+        System.out.println("Plytos kaina Eur: ");
+        String kaina = scannner.nextLine();
+        System.out.println(kaina);
+
+        int ilgis1 = Integer.parseInt(ilgis);
+        int aukstis1 = Integer.parseInt(aukstis);
+        double kaina1 = Double.valueOf(kaina);
+
+        int s = (ilgis1 * aukstis1) * 10000;
+        int kiekis = s / (20 * 10);
+
+        System.out.println("Plytų kiekis" + " " + kiekis);
+
+        double kainuos = kiekis * kaina1;
+        System.out.println("Plytos kainuos" + " " +kainuos+ " " + "Eur");
+    }
+}
