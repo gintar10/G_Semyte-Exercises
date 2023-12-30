23a786719d107f36d86aafb341ef8e43ff8acb02
gintar10 <151562195+gintar10@users.noreply.github.com>
2023-12-30 15:45:40 +0200

    Update PD4.7

--- G_Semyte-Exercises/PD4/PD4.7/PD4_7.java
+++ G_Semyte-Exercises/PD4/PD4.7/PD4_7.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class PD4_7 {
     public static void main(String[] args) {
-        Scanner scanNumbers1 = new Scanner(System.in);
+        
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Kiek paskaitu yra pirmadieni ");
-        int p = scanNumbers1.nextInt();
+        int p = scanner.nextInt();
 
-        Scanner scanNumbers2 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
         System.out.println("Kiek paskaitu yra antradieni ");
-        int a = scanNumbers2.nextInt();
+        int a = scanner2.nextInt();
 
-        Scanner scanNumbers3 = new Scanner(System.in);
+        Scanner scanner3 = new Scanner(System.in);
         System.out.println("Kiek paskaitu yra treciadieni ");
-        int t = scanNumbers3.nextInt();
+        int t = scanner3.nextInt();
 
-        Scanner scanNumbers4 = new Scanner(System.in);
+        Scanner scanner4 = new Scanner(System.in);
         System.out.println("Kiek paskaitu yra ketvirtadieni ");
-        int k = scanNumbers4.nextInt();
+        int k = scanner4.nextInt();
 
-        Scanner scanNumbers5 = new Scanner(System.in);
+        Scanner scanners5 = new Scanner(System.in);
         System.out.println("Kiek paskaitu yra penktadieni ");
-        int pnk = scanNumbers5.nextInt();
+        int pnk = scanners5.nextInt();
 
         double savaite = p + a + t + k + pnk;
         System.out.println("Paskaitu skaicius per savaite -" + " " + savaite);
