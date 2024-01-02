1ca7c310e985783266106c4b1ed63ce51f39137d
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-02 17:40:55 +0200

    Add PD5.3

---
+++ G_Semyte-Exercises/PD5/PD5.3/PD5_3.java
@@ -0,0 +1,22 @@
+package PD5;
+
+public class PD5_3 {
+    public static void main(String[] args) {
+        int i, primenumber, count;
+
+        System.out.println(" Prime Numbers : ");
+        for(primenumber = 1; primenumber >= 1; primenumber++) {
+            count = 0;
+            for (i = 2; i <= primenumber/2; i++) {
+                while (primenumber % i == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0 && primenumber != 1 ){
+                System.out.print(primenumber + " ");
+            }
+        }
+    }
+}
+
