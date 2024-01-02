3d36d7da0ba3f6e69041219b56b888b5216dc3e6
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-02 16:34:03 +0200

    Add PD5.2

---
+++ G_Semyte-Exercises/PD5/PD5.2/PD5_2.java
@@ -0,0 +1,15 @@
+package PD5_0;
+
+public class PD5_2 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        for (a = 1; a <= 10; a++) {
+            for (b = 1; b <= 10; b++) {
+                System.out.print(a * b + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
