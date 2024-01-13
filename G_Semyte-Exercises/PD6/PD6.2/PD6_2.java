0b56e57b2619c84e766e0910d54783cae35c1e4b
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-13 17:30:08 +0200

    Add PD6.2

---
+++ G_Semyte-Exercises/PD6/PD6.2/PD6_2.java
@@ -0,0 +1,14 @@
+package lt.techin.pd6;
+
+import java.util.Arrays;
+
+public class PD6_2 {
+    public static void main(String[] args) {
+        int[] originalArray = {1, 2, 3, 4, 5, -10};
+        String[] names = {"Tomas", "Akvile", "Jonas"};
+        System.out.println(originalArray[0]);
+        System.out.println(originalArray[originalArray.length - 1]);
+        System.out.println(names[0]);
+        System.out.println(names[names.length - 1]);
+    }
+}
