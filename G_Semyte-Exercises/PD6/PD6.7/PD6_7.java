2027a5fb16fdbc37a4cd071dd593c7fd3a19c926
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-13 21:03:19 +0200

    Add PD6.7

---
+++ G_Semyte-Exercises/PD6/PD6.7/PD6_7.java
@@ -0,0 +1,17 @@
+
+package lt.techin.pd6;
+
+public class PD6_7 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, -3, 1, -2, 3, 4, 3, 5, -10, 3};
+        int sum = 0;
+        int multiplication = 1;
+
+        for (int element : numbers){
+            sum += element;
+            multiplication *= element;
+        }
+        System.out.println("Sum: " + sum);
+        System.out.println("Multiplication: " + multiplication);
+    }
+}
