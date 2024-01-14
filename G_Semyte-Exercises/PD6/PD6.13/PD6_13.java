8207aa92806141829dfc344712e9bfb1617a3809
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-14 17:54:54 +0200

    Add PD6.13

---
+++ G_Semyte-Exercises/PD6/PD6.13/PD6_13.java
@@ -0,0 +1,20 @@
+package lt.techin.pd6;
+
+public class PD6_13 {
+    public static void main(String[] args){
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        int[] numbers2 = {1, 2, 3, 5, 5};
+        boolean areEqual = true;
+        if (numbers1.length != numbers2.length){
+            areEqual = false;
+        } else {
+            for (int i = 0; i < numbers1.length; i++){
+                if (numbers1[i] != numbers2[i]){
+                    areEqual = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(areEqual);
+    }
+}
