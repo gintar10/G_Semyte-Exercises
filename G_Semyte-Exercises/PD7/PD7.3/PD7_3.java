d11e3c205bd652d2dc6d6b25a7950663ab992c6a
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-16 20:41:19 +0200

    Add PD7.3

---
+++ G_Semyte-Exercises/PD7/PD7.3/PD7_3.java
@@ -0,0 +1,11 @@
+package lt.techin.pd7;
+
+public class PD7_3 {
+    public static void main(String[] args) {
+        System.out.println(getTriangleArea(10));
+    }
+
+    public static int getTriangleArea (int number){
+            return (number * number)/2;
+    }
+}
