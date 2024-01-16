71770b4fa306c5399d5496cfaf18c314ad1d7fd3
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-16 20:29:15 +0200

    Add PD7.1

---
+++ G_Semyte-Exercises/PD7/PD7.1/PD7_1.java
@@ -0,0 +1,10 @@
+package lt.techin.pd7;
+
+public class PD7_1 {
+    public static void main(String[] args) {
+        System.out.println(getNumPow(10));
+    }
+    public static int getNumPow (int number) {
+        return number * number;
+    }
+}
