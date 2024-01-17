931e870a73b22e4a03b3ae185c72b6db2c877770
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-17 15:35:28 +0200

    Add PD7.9

---
+++ G_Semyte-Exercises/PD7/PD7.9/PD7_9.java
@@ -0,0 +1,15 @@
+package lt.techin.pd7;
+
+import java.util.Arrays;
+
+public class PD7_9 {
+    public static void main(String[] args) {
+        String text = "The last example for today ...";
+        System.out.println(sortedChars(text));
+    }
+    public static String sortedChars(String text) {
+        char[] charArray = text.toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
+}
