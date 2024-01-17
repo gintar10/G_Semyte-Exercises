94fe4501a50779c063b0cd880f94662de0fabdef
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-17 13:48:03 +0200

    Add PD7.5

---
+++ G_Semyte-Exercises/PD7/PD7.5/PD7_5.java
@@ -0,0 +1,21 @@
+package lt.techin.pd7;
+
+import java.math.BigInteger;
+
+public class PD7_5 {
+    public static void main(String[] args) {
+        int number = 120;
+        System.out.println(countNumberFactorial(number));
+    }
+    public static BigInteger countNumberFactorial(int number) {
+        if (number < 0) {
+            return BigInteger.ZERO;
+        } else {
+            BigInteger factorial = BigInteger.ONE;
+            for (int i = 1; i <= number; i++) {
+                factorial = factorial.multiply(BigInteger.valueOf(i));
+            }
+            return  factorial;
+        }
+    }
+}
