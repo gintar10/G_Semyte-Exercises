5a87812e7cf4fa1312b63d1ecfc76ac57551feb6
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-19 17:07:36 +0200

    Add PD7.10

---
+++ G_Semyte-Exercises/PD7/PD7.10/PD7_10.java
@@ -0,0 +1,21 @@
+package lt.techin.pd7;
+
+public class PD7_10 {
+    public static void main(String[] args) {
+        String text = "hello world";
+        System.out.println(capitalizeFirstOfWord(text));
+
+    }
+    private static String capitalizeFirstOfWord(String text) {
+        String[] words = text.split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (String word : words){
+            if (!word.isEmpty()){
+                result.append(Character.toUpperCase(word.charAt(0)));
+                result.append(word.substring(1));
+                result.append(" ");
+            }
+        }
+        return result.toString().trim();
+    }
+}
