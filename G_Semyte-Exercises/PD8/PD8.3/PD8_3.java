3be0bf9131c2d248a0fe73692d3428767d0bf11c
gintar10 <151562195+gintar10@users.noreply.github.com>
2024-01-20 16:14:10 +0200

    Add PD8.3

---
+++ G_Semyte-Exercises/PD8/PD8.3/PD8_3.java
@@ -0,0 +1,8 @@
+package lt.techin.pd8;
+
+public class PD8_3 {
+    public static void main(String[] args) {
+        HexEncoder hexEncoder = new HexEncoder();
+        System.out.println(hexEncoder.encodeToHex("I love Java :)"));
+    }
+}
